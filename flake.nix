e3f4fbf2720e9cbaa3a7defe802bc7ed1f6bd271
eljamm <fedi.jamoussi@protonmail.ch>
2024-05-31 23:43:08 +0100

    Add linux as a flake system and refactor code

--- flake.nix
+++ flake.nix
@@ -4,20 +4,38 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    utils.lib.eachSystem supportedSystems (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         version = "0.0.0.3";
         releasesUrl = "https://github.com/kamalsacranie/anki-panky/releases";
+        src =
+          if system == "x86_64-darwin" then
+            pkgs.fetchzip {
+              url = "${releasesUrl}/download/${version}/macOS-11-anki-panky-${version}.tar.gz";
+              sha256 = "sha256-HJX0C7YKTYzbkCUnww3N2VBRcMNhGvvwBX0YBTnO5Us=";
+            }
+          else
+            pkgs.fetchzip {
+              url = "${releasesUrl}/download/${version}/ubuntu-latest-anki-panky-${version}.tar.gz";
+              sha256 = "sha256-0NlgyA/HWVDADj21K17xGqiRx9kIpx/5eOjOgQL6tsA=";
+            };
         ankiPanky = pkgs.stdenv.mkDerivation {
           name = "anki-panky";
-          version = "0.0.0.3";
-          src = pkgs.fetchzip {
-            url = "${releasesUrl}/download/${version}/macOS-11-anki-panky-${version}.tar.gz";
-            sha256 = "HJX0C7YKTYzbkCUnww3N2VBRcMNhGvvwBX0YBTnO5Us=";
-          };
-          phases = [ "installPhase" ];
+          inherit src version;
           installPhase = ''
             mkdir -p $out/bin
             cp -R $src/anki-panky $out/bin/anki-panky
@@ -26,17 +44,23 @@
         };
         fonts = pkgs.nerdfonts.override { fonts = [ "Hasklig" ]; };
       in
+      with pkgs;
       {
-        devShells.default = with pkgs; mkShell {
-          nativeBuildInputs = [
-            fonts
-          ];
+        devShells.default = mkShell {
+          nativeBuildInputs = [ fonts ];
 
           buildInputs = [
             ankiPanky
             pandoc
             tectonic
           ];
+
+          # Add necessary library dependencies
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath [
+            gmp
+            zlib
+          ]}";
         };
-      });
+      }
+    );
 }
